64455c9d78c0a31cda4cdd0f8325598c4e460998
SahooBishwajeet <biswa.2004.bdk@gmail.com>
2025-02-08 22:09:15 +0530

    [Add] ESLint and Prettier configuration files; update package.json for linting and formatting

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-import dotenv from "dotenv";
-import express, { Application, Request, Response } from "express";
+import dotenv from 'dotenv';
+import express, { Application, Request, Response } from 'express';
 
 //For env File
 dotenv.config();
@@ -7,10 +7,10 @@ dotenv.config();
 const app: Application = express();
 const port = process.env.PORT;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Welcome to Express & TypeScript Server");
+app.get('/', (req: Request, res: Response) => {
+    res.send('Welcome to Express & TypeScript Server');
 });
 
 app.listen(port, () => {
-  console.log(`Server is Fire at http://localhost:${port}`);
+    console.log(`Server is Fire at http://localhost:${port}`);
 });
